28dfdcd9e0533dfbda674f8b6b7ca3cd0cee5b35
James Rodden <james.rodden@savanta.com>
2022-12-02 11:22:19 +0000

    Day 2 part 2

--- 2/rockPaperScissors.hs
+++ 2/rockPaperScissors.hs
@@ -6,30 +6,36 @@ main = do
     strategy <- readFile "strategy.txt"
     print $ sum $ map calculateScore $ stringToTuples strategy
 
-
 calculateScore :: (Char, Char) -> Int
-calculateScore (a, b) = rockPaperScissorsScore a b + scoreForShape b
+calculateScore (a, b) =  scoreForRound b + (scoreForShape $ rockPaperScissorsChoice a b)
 
-rockPaperScissorsScore :: Char -> Char -> Int
-rockPaperScissorsScore 'A' b
-    | b == 'X' = 3
-    | b == 'Y' = 6
-    | b == 'Z' = 0
+rockPaperScissorsChoice :: Char -> Char -> Char
+rockPaperScissorsChoice a 'X' = loses a
+rockPaperScissorsChoice a 'Y' = ties a
+rockPaperScissorsChoice a 'Z' = beats a
 
-rockPaperScissorsScore 'B' b
-    | b == 'X' = 0
-    | b == 'Y' = 3
-    | b == 'Z' = 6
+beats :: Char -> Char
+beats 'A' = 'B'
+beats 'B' = 'C'
+beats 'C' = 'A'
 
-rockPaperScissorsScore 'C' b
-    | b == 'X' = 6
-    | b == 'Y' = 0
-    | b == 'Z' = 3
+ties :: Char -> Char
+ties c = c
+
+loses :: Char -> Char
+loses 'A' = 'C'
+loses 'B' = 'A'
+loses 'C' = 'B'
 
 scoreForShape :: Char -> Int
-scoreForShape 'X' = 1
-scoreForShape 'Y' = 2
-scoreForShape 'Z' = 3
+scoreForShape 'A' = 1
+scoreForShape 'B' = 2
+scoreForShape 'C' = 3
+
+scoreForRound :: Char -> Int
+scoreForRound 'X' = 0
+scoreForRound 'Y' = 3
+scoreForRound 'Z' = 6
 
 stringToTuples :: String -> [(Char, Char)]
 stringToTuples input = map stringToTuple $ map (filter (\x -> not $ isSpace x)) $ lines input
