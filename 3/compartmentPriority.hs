06c3d746d51f4a280d07875e2b7b3cf137a205ea
James Rodden <james.rodden@savanta.com>
2022-12-03 18:27:17 +0000

    Day 3 part 2

--- 3/compartmentPriority.hs
+++ 3/compartmentPriority.hs
@@ -4,16 +4,23 @@ import Data.List
 
 main = do
     compartments <- readFile "compartments.txt"
-    print $ sum $ map priorityOfSharedItem $ lines compartments
+    print $ sum $ map priorityOfBadge $ groupsOf3 $ lines compartments
+
+groupsOf3 :: [String] -> [(String, String, String)]
+groupsOf3 [] = []
+groupsOf3 (a:b:c:xs) = [(a, b, c)] ++ groupsOf3 xs
+
+priorityOfBadge :: (String, String, String) -> Int
+priorityOfBadge (x, y, z) = priority $ head $ intersect x $ intersect y z
 
 priorityOfSharedItem :: String -> Int
 priorityOfSharedItem c = priority $ head $ intersect (firstHalfOfList c) (lastHalfOfList c)
 
 priority :: Char -> Int
 priority c
-    | c <= 'Z' = ord c - 38
-    | c <= 'z' = ord c - 96
-    | otherwise  = 0
+    | c <= 'Z'  = ord c - 38
+    | c <= 'z'  = ord c - 96
+    | otherwise = 0
 
 -- Assume list has even length
 firstHalfOfList :: [a] -> [a]
