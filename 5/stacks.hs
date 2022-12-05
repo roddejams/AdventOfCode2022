aeb95640ed389081a85e4a1978fffe314ac38f5e
James Rodden <james.rodden@savanta.com>
2022-12-05 14:05:58 +0000

    group up functions a bit better

--- 5/stacks.hs
+++ 5/stacks.hs
@@ -9,20 +9,11 @@ main = do
     print $ head moves
     print $ readMessage $ runMoves supply moves
 
-parseInput :: String -> (Supply, [Move])
-parseInput input = (stringsToSupply supplyStrings, map parseMove instructions)
-    where supplyStrings = lines $ head inputLines
-          instructions = lines $ last inputLines
-          inputLines = splitOn "\n\n" input
-
 readMessage :: Supply -> [Crate]
 readMessage supply = map (head . stack) supply
 
-data Move = Move {count :: Int, sourceIndex :: Int, destinationIndex :: Int} deriving (Show)
-
-parseMove :: String -> Move
-parseMove input = Move (read $ splitInput !! 1) (read $ splitInput !! 3) (read $ splitInput !! 5)
-    where splitInput = words input
+runMoves :: Supply -> [Move] -> Supply
+runMoves supply moves = foldl applyMoveToSupplyKeepOrder supply moves
 
 applyMoveToSupply :: Supply -> Move -> Supply
 applyMoveToSupply supply (Move 0 _ _) = supply
@@ -40,31 +31,42 @@ takeCratesFromSupplyAtIndex count index supply = (crate:rest, finalSupply)
     where (crate, finalSupply) = popCrateFromSupplyAtIndex index modifiedSupply
           (rest, modifiedSupply) = takeCratesFromSupplyAtIndex (count - 1) index supply 
 
-runMoves :: Supply -> [Move] -> Supply
-runMoves supply moves = foldl applyMoveToSupplyKeepOrder supply moves
+pushCrateToStackAtIndex :: Int -> Supply -> Crate -> Supply
+pushCrateToStackAtIndex i stacks newCrate = map (\x -> if (index x) == i then pushCrateToStack newCrate x else x) stacks
+
+popCrateFromSupplyAtIndex :: Int -> Supply -> (Crate, Supply)
+popCrateFromSupplyAtIndex i supply = (crate, map (\x -> if (index x) == i then poppedStack else x) supply)
+    where (crate, poppedStack) = popCrateFromStack (supply !! (i - 1))
+
+pushCrateToStack :: Crate -> IndexedStack -> IndexedStack
+pushCrateToStack Empty stack = stack
+pushCrateToStack newCrate (IndexedStack i stack) = IndexedStack i (newCrate:stack)
+
+popCrateFromStack :: IndexedStack -> (Crate, IndexedStack)
+popCrateFromStack (IndexedStack i stack) = (head stack, IndexedStack i $ tail stack)
+
+-------------------------------------------------------------------------------------------------
+
+data Move = Move {count :: Int, sourceIndex :: Int, destinationIndex :: Int} deriving (Show)
 
 data Crate = Crate Char | Empty deriving (Show, Eq, Ord)
 type Stack = [Crate]
 data IndexedStack = IndexedStack { index :: Int, stack :: Stack} deriving (Show)
 type Supply = [IndexedStack]
 
+parseInput :: String -> (Supply, [Move])
+parseInput input = (stringsToSupply supplyStrings, map parseMove instructions)
+    where supplyStrings = lines $ head inputLines
+          instructions = lines $ last inputLines
+          inputLines = splitOn "\n\n" input
+
+parseMove :: String -> Move
+parseMove input = Move (read $ splitInput !! 1) (read $ splitInput !! 3) (read $ splitInput !! 5)
+    where splitInput = words input
+
 stringsToSupply :: [String] -> Supply
 stringsToSupply (list:indexes:[]) = zipWith (IndexedStack) (map (read) $ words indexes) (map (:[]) $ stringToCrateList list)
 stringsToSupply (list:rest) = zipWith (pushCrateToStack) (stringToCrateList list) (stringsToSupply rest)
-
-pushCrateToStackAtIndex :: Int -> Supply -> Crate -> Supply
-pushCrateToStackAtIndex i stacks newCrate = map (\x -> if (index x) == i then pushCrateToStack newCrate x else x) stacks
-
-pushCrateToStack :: Crate -> IndexedStack -> IndexedStack
-pushCrateToStack Empty stack = stack
-pushCrateToStack newCrate (IndexedStack i stack) = IndexedStack i (newCrate:stack)
-
-popCrateFromSupplyAtIndex :: Int -> Supply -> (Crate, Supply)
-popCrateFromSupplyAtIndex i supply = (crate, map (\x -> if (index x) == i then poppedStack else x) supply)
-    where (crate, poppedStack) = popCrateFromStack (supply !! (i - 1))
-
-popCrateFromStack :: IndexedStack -> (Crate, IndexedStack)
-popCrateFromStack (IndexedStack i stack) = (head stack, IndexedStack i $ tail stack)
 
 stringToCrateList :: String -> [Crate]
 stringToCrateList [] = []
