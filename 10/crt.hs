a05036914fc0ea1636c27f2c470f7d74a2e0da76
James Rodden <james.rodden@savanta.com>
2022-12-14 09:29:43 +0000

    Day 10 part 1

--- 10/crt.hs
+++ 10/crt.hs
@@ -1,25 +1,36 @@
 import Data.List (isPrefixOf)
 import System.IO ()
+import Data.List (find)
 
 main = do
-    programInput <- readFile "program.txt"
-    let program = parseProgram programInput
-    let output = runProgram program id 1
-    print $ take 20 output
+    program <-readFile "program.txt"
+    let output = runProgram $ lines program
+    print output
+    let strengths = map (\c -> (signalStrengthAtCycle output c) * c) ([20,60,100,140,180,220]) 
+    print $ sum strengths
 
-runProgram' :: [[Int -> Int]] -> Int -> [Int]
-runProgram' program x = map fst $ scanl (\(acc, f) instr -> ((head instr . f) x, (last instr))) (x, id) (program ++ [[id], [id], [id]])
+signalStrengthAtCycle :: [(Int, Int)] -> Int -> Int
+signalStrengthAtCycle output cycleNumber = signalStrength
+    where signalStrength = case find (\(x, c) -> c == cycleNumber) output of (Just (x, c)) -> x
+                                                                             Nothing -> signalStrengthAtCycle output (cycleNumber - 1) 
 
-runProgram :: [[Int -> Int]] -> (Int -> Int) -> Int -> [Int]
-runProgram [] f x = [f x]
-runProgram ([]:program) f x = [f x] ++ (runProgram program id $ f x)
-runProgram (instr:program) f x = [nextValue] ++ (runProgram program (last instr) $ nextValue)
-    where nextValue = (head instr . f) x
 
-parseProgram :: String -> [[Int -> Int]]
-parseProgram = map parseInstruction . lines
+runProgram :: [String] -> [(Int, Int)]
+runProgram program = scanl parseInstruction (1, 1) program
 
-parseInstruction :: String -> [Int -> Int]
-parseInstruction inst 
-    | isPrefixOf "addx" inst = [id, (+ (read $ drop 4 inst))]
-    | otherwise = [id]
+-- runProgram' :: [[Int -> Int]] -> Int -> [Int]
+-- runProgram' program x = map fst $ scanl (\(acc, f) instr -> ((head instr . f) x, (last instr))) (x, id) (program ++ [[id], [id], [id]])
+
+-- runProgram :: [[Int -> Int]] -> (Int -> Int) -> Int -> [Int]
+-- runProgram [] f x = [f x]
+-- runProgram ([]:program) f x = [f x] ++ (runProgram program id $ f x)
+-- runProgram (instr:program) f x = [nextValue] ++ (runProgram program (last instr) $ nextValue)
+--     where nextValue = (head instr . f) x
+
+-- parseProgram :: String -> Int -> Int
+-- parseProgram = map parseInstruction . lines
+
+parseInstruction :: (Int, Int) -> String -> (Int, Int)
+parseInstruction (x, cycle) inst 
+    | isPrefixOf "addx" inst = (x + (read $ drop 4 inst), cycle + 2)
+    | otherwise = (x, cycle + 1)
